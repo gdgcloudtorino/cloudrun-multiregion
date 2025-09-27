edf5bbaf7c67c84bfdb8ff40a2447d93863f3da8
c.francesco.p <c.francesco.p@gmail.com>
2025-09-27 10:07:43 +0000

    cleanup

--- game-api/output.tf
+++ game-api/output.tf
@@ -1,7 +1,5 @@
 output "endpoint" {
   value = google_cloud_run_v2_service.service.urls[0]
 }
-output "uri" {
-  value = google_cloud_run_v2_service.service.uri
-}
+
 
--- gcs-proxy/output.tf
+++ gcs-proxy/output.tf
@@ -2,6 +2,4 @@ output "endpoint" {
   value = google_cloud_run_v2_service.service.urls[0]
 }
 
-output "uri" {
-  value = google_cloud_run_v2_service.service.uri
-}
+
